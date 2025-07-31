d8082bfc77f934f78f3e1437f5a2cdb6a182cebc
Kathryn.Bray@formenteraops.com <Kathryn.Bray@formenteraops.com>
2025-07-31 21:12:29 +0000

    Add Target Fields and clean up system fields in production targets staging model

--- models/operations/staging/prodview/stg_prodview_production_targets.sql
+++ models/operations/staging/prodview/stg_prodview_production_targets.sql
@@ -18,11 +18,16 @@ renamed as (
         -- Date/Time information
         dttmstart as "Target Start Date",
         
-        - - 
+        -- Target Fields
+        typ as "Target Type",
+        usecalcdiff as "Is Use in Diff from Target Calculations",
+        usertxt1 as "CC Forecast Name",
+        usertxt2 as "User Text 2",
+        usertxt3 as "User Text 3",
+
         -- System fields
         syscreatedate as "Created At",
         syscreateuser as "Created By",
-        sysModDateLocal as "Last Mod Date"
         sysmoddate as "Last Mod At (UTC)",
         sysmoduser as "Last Mod By",
         systag as "System Tag",
