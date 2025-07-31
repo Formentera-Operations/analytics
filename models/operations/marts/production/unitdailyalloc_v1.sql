d2ab11848f7ea2a34eddc826267528c62a7b05b5
Kathryn.Bray@formenteraops.com <Kathryn.Bray@formenteraops.com>
2025-07-31 18:46:22 +0000

    refactor: consolidate and simplify daily allocation models using new production volumes view
    The commit reorganizes the daily allocation models by removing the complex intermediate model and replacing it with a simplified production volumes view, while maintaining the same data output.

--- models/operations/marts/production/unitdailyalloc_v1.sql
+++ models/operations/marts/production/unitdailyalloc_v1.sql
@@ -1,5 +1,5 @@
 {{
-  config(
+  config( 
     materialized='incremental',
     unique_key='IDREC',
     on_schema_change='fail',
@@ -7,7 +7,7 @@
   )
 }}
 
-select * from {{ ref('int_prodview__unitdailyalloc_v1') }}
+select * from {{ ref('int_prodview__production_volumes') }}
 
 {% if is_incremental() %}
   -- this filter will only be applied on an incremental run
