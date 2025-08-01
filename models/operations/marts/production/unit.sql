46e28719092246c4b9aa17d1d162904cba4d434d
Kathryn.Bray@formenteraops.com <Kathryn.Bray@formenteraops.com>
2025-08-01 15:06:42 +0000

    Remove legacy ProdView models and ODA wells staging file [Data Cleanup] chore: cleanup legacy models from operations mart and staging layers

--- models/operations/marts/production/unit.sql
+++
@@ -1,8 +0,0 @@
-{{
-  config(
-    materialized='table',
-    alias='pvunit'
-  )
-}}
-
-select * from {{ ref('stg_prodview__units') }}
--- models/operations/marts/production/unitallocmonth.sql
+++
@@ -1,8 +0,0 @@
-{{
-  config(
-    materialized='table',
-    alias='pvunitallocmonth'
-  )
-}}
-
-select * from {{ ref('stg_prodview__monthly_allocations') }}
