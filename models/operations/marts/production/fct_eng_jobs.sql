b3a32c766f8815f573ea8a380ad17c286181aab0
Kathryn.Bray@formenteraops.com <Kathryn.Bray@formenteraops.com>
2025-09-23 19:02:17 +0000

    Add job date fields and create economics view for commodity pricing

--- models/operations/marts/production/fct_eng_jobs.sql
+++ models/operations/marts/production/fct_eng_jobs.sql
@@ -8,7 +8,9 @@ WITH jobs as (
     Select
         "Job ID"
         ,"Job End Datetime"
+        ,CAST("Job End Datetime" as date) AS "Job End Date"
         ,"Job Start Datetime"
+        ,CAST("Job Start Datetime" as date) AS "Job Start Date"
         ,"Planned Start Datetime"
         ,"Primary Job Type"
         ,"Well ID"
