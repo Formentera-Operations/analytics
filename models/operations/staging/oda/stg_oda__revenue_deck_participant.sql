0535932c43c133d11cbd516702ce5241607d9eb9
michael.shiraz@formenteraops.com <michael.shiraz@formenteraops.com>
2025-12-09 20:47:57 +0000

    Add ODA revenue deck participant staging model
    Creates stg_oda__revenue_deck_participant with renamed columns and proper structure for revenue deck participant data from ODA source.

---
+++ models/operations/staging/oda/stg_oda__revenue_deck_participant.sql
@@ -0,0 +1,42 @@
+with source as (
+    
+    select * from {{ source('oda', 'ODA_BATCH_ODA_REVENUEDECKPARTICIPANT') }}
+
+),
+
+renamed as (
+
+    select
+        -- Primary key and deck identifiers
+        ID as id,
+        DECKREVISIONID as deck_revision_id,
+        REVENUEDECKPARTICIPANTIDENTITY as expense_deck_participant_identity,
+        ISMEMO as is_memo,
+        
+        -- Entity details
+        ENTITYTYPEID as entity_type_id,
+        COMPANYID as company_id,
+        OWNERID as owner_id,
+        
+        -- Interest and Suspense information
+        INTERESTTYPEID as interest_type_id,
+        DECIMALINTEREST as decimal_interest,
+        CUSTOMINTERESTTYPEID as custom_interest_type_id,
+        AUTOSUSPENDPAYMENT as auto_suspend_payment,
+        SUSPENSECATEGORYID as suspend_category_id, 
+        
+        -- Metadata and timestamps
+        CREATEDATE as create_date,
+        CREATEEVENTID as create_event_id,
+        UPDATEDATE as update_date,
+        UPDATEEVENTID as update_event_id,
+        FLOW_PUBLISHED_AT as flow_published_at,
+        
+        -- Full document JSON for reference
+        FLOW_DOCUMENT as flow_document
+
+    from source
+
+)
+
+select * from renamed
