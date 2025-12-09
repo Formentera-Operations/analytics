75be3bbb81984cb03d5417ef98cc8dd1629a7800
michael.shiraz@formenteraops.com <michael.shiraz@formenteraops.com>
2025-12-09 19:53:42 +0000

    Removing MemoCompanyID Column

--- models/operations/staging/oda/stg_oda__expense_deck_participant.sql
+++ models/operations/staging/oda/stg_oda__expense_deck_participant.sql
@@ -11,6 +11,7 @@ renamed as (
         ID as id,
         DECKREVISIONID as deck_revision_id,
         EXPENSEDECKPARTICIPANTIDENTITY as expense_deck_participant_identity,
+        ISMEMO as is_memo,
         
         -- Entity details
         ENTITYTYPEID as entity_type_id,
@@ -22,10 +23,6 @@ renamed as (
         DECIMALINTEREST as decimal_interest,
         CUSTOMINTERESTTYPEID as custom_interest_type_id,
 
-        --Memo
-        ISMEMO as is_memo,
-        MEMOCOMPANYID as memo_company_id, 
-        
         -- Metadata and timestamps
         CREATEDATE as create_date,
         CREATEEVENTID as create_event_id,
